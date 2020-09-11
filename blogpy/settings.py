ca5011bbb36aeb0c492680bc4dc37107534d8e50
Ali Pishgard <alipishgard79@gmail.com>
2020-09-11 21:32:05 +0430

    Initial commit

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'Ali1991379',
         'HOST': 'localhost',
         'PORT': '5432',
     }

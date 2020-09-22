7aab3e43a405de7d7844ae5a2a7173ac2dd021c3
Ali Pishgard <alipishgard79@gmail.com>
2020-09-22 23:08:28 +0330

    Initiall commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(UserProfile)
-admin.site.register(Article)
-admin.site.register(Category)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'avatar', 'description']
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
+class ArticalAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ['title', 'category', 'created_at']
+
+admin.site.register(Article, ArticalAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'cover']
+
+admin.site.register(Category,CategoryAdmin)

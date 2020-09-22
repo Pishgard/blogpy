daf8fa4cef9e6314ed4deec509fb47955f0b7825
Ali Pishgard <alipishgard79@gmail.com>
2020-09-22 21:14:16 +0330

    Initial commit

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import User
+from ckeditor.fields import RichTextField
+from datetime import datetime
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,3 +11,11 @@ class UserProfile(models.Model):
 class Article(models.Model):
      title = models.CharField(max_length=128, null=False, blank=False)
      cover = models.FileField(upload_to='filed/article_cover/', null=False, blank=False)
+     content = RichTextField()
+     created_at = models.DateTimeField(default=datetime, blank=False)
+     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+     author = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    title = models.CharField(max_length=128, null=False, blank=False)
+    cover = models.FileField(upload_to='files/category_cover/',null=False, blank=False)

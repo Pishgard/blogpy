4e9e42a6e8c0aaa050f130e6404215621279bcc6
Ali Pishgard <alipishgard79@gmail.com>
2020-09-22 20:38:55 +0330

    Initial commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.FileField(upload_to='files/user_avatar/', null=True, blank=True)
+    description = models.CharField(max_length=512, null=False, blank=False)
+
+class Article(models.Model):
+     title = models.CharField(max_length=128, null=False, blank=False)
+     cover = models.FileField(upload_to='filed/article_cover/', null=False, blank=False)
